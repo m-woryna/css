de8455ac207bd8886ca16bdfb986732de5ad11e8
m-woryna <58751570+m-woryna@users.noreply.github.com>
2020-02-04 09:10:34 +0100

    Add files via upload

---
+++ przejazdy_przeloty_linki.php
@@ -0,0 +1,59 @@
+<HTML>
+<HEAD>
+
+<META HTTP-EQUIV="Content-type" CONTENT="text/html; charset=iso-8859-2">
+<style>
+H1 {font-family:sans-serif; font-weight:bold; font-size:20px; color:#1b2a5b;}
+H2 {font-family:sans-serif; font-weight:bold; font-size:17px; color:#1b2a5b;}
+H3 {font-family:sans-serif; font-weight:bold; font-size:15px; color:#1b2a5b;}
+.linki {
+  font-size: medium;
+  font-family: Arial, Helvetica, sans-serif;
+   line-height: 1.2;
+  
+}
+.linki a {
+    font-weight: bold;
+    text-decoration: underline;
+    word-break: break-all;
+ }
+</style>
+
+</HEAD>
+<? // turystyka-atrakcje.pl, przejazdy, przeloty, rejsy, przewożnicy >
+
+error_reporting(0);
+$domenat='http://turystyka-atrakcje.pl/';
+
+
+$turystyka_przejazdy_przeloty='<div class="linki"><br><h2>
+PRZEJAZDY, PRZELOTY, REJSY - LINKI:  </h2><br>
+
+<IMG SRC="foto/costa_victoria1i.jpg"  style="width:94%" BORDER="1" ALT="turystyka-atrakcje , rejsy, przejazdy, przeloty, statek Costa Victoria"><br><B>Statek  Costa Victoria, port w Neapolu</B><br>
+Fot.  Carlo Mirante/CC 2.0<br><br>
+
+<a HREF="http://www.rezerwacja.pl/" target="_blank">http://www.rezerwacja.pl/</A> - Bilety lotnicze - rezerwacja biletów.<br>
+<a HREF="http://www.esky.pl/" target="_blank" title="katalog stron turystyka-atrakcje.pl, przeloty esky.pl">http://www.esky.pl/</A> - eSKY.pl - Centrum Podróży - Bilety lotnicze, Hotele, Wczasy. 650 Linii Lotniczych ˇ 65 500 Destynacji ˇ 3 000 Lotnisk.<br><BR>
+
+<a HREF="http://www.biletyautokarowe.pl/" target="_blank" title="katalog stron turystyka-atrakcje.pl, biletyautokarowe.pl">http://www.biletyautokarowe.pl/</A> - Bilety autokarowe na międzynarodowe linie autokarowe.<br>
+<a HREF="http://www.autokary24.pl/" target="_blank" title="katalog stron turystyka-atrakcje.pl, autokarzy24.pl">http://www.autokary24.pl/</A> - Sprzedaż biletów autokarowych 24h na połączenia międzynarodowe po całej Europie.<br>
+<a HREF="http://www.sindbad.pl/" target="_blank" title="katalog stron turystyka-atrakcje.pl, sindbad.pl">http://www.sindbad.pl/</A> - Międzynarodowe przewozy autokarowe.<br>
+<a HREF="http://www.liput-tour.pl/" target="_blank" title="katalog stron turystyka-atrakcje.pl, liput-tour.pl">http://www.liput-tour.pl/</A> - LIPUT-TOUR przewozy bus, przewozy Polska Niemcy, Holandia, Belgia.<br>
+<BR>
+<a href="http://rozklad.pkp.pl/" TITLE="turystyka-atrakcje rozkład jazdy PKP" target="blank">http://rozklad.pkp.pl/</a> - rozkład jazdy PKP.<br>
+<a href="http://www.intercity.pl/" TITLE="turystyka-atrakcje rozkład jazdy Intercity" target="blank">http://www.intercity.pl/</a> - PKP INTERCITY.<br>
+<br>
+
+<a HREF="http://www.rejsy.pl/" target="_blank">http://www.rejsy.pl/</A> - Rejsy wycieczkowe MARCO POLO TRAVEL.<br>
+<a HREF="http://www.namorzu.pl/" target="_blank" title="katalog stron turystyka-atrakcje.pl, rejsy namorzu.pl">http://www.namorzu.pl/</A> - Rejsy wycieczkowe po morzach i oceanach. Rejsy największymi statkami świata - oryginalne i niepowtarzalne wakacje na morzu.<br>
+<a HREF="http://www.stenaline.pl/" target="_blank" title="katalog stron turystyka-atrakcje.pl, przejazdy, stenaline.pl">http://www.stenaline.pl/</A> - Stena Line - Promy do Szwecji, UK oraz Irlandii.<br>
+<br>
+
+<B>Carpooling - wspólne przejazdy: </B><br>
+Wyjeżdżasz samochodem w podróż i masz wolne miejsca - zabierz osoby jadące w tym samym kierunku.
+<a HREF="http://autem.pl/">http://autem.pl/</A> - carpooling po polsku, wspólne dojazdy, wolne miejsce, wspólne przejazdy, dojazdy do pracy.<br><br>
+
+</div>';
+
+print $turystyka_przejazdy_przeloty;
+?>
